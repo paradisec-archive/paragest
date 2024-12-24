b67ac15d4d3ddd11f3903e4102a515a617bb7aee
John Ferlito <johnf@inodes.org>
2024-12-24 15:15:15 +1100

    Better error on token

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -21,20 +21,26 @@ const getAccessToken = async (credentials: OAuthSecret): Promise<string> => {
     scope: 'read admin',
   };
 
-  const tokenResponse = await fetch(tokenUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(tokenRequestData),
-  });
-  const tokenData = (await tokenResponse.json()) as { access_token: string };
+  try {
+    const tokenResponse = await fetch(tokenUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(tokenRequestData),
+    });
 
-  if (!tokenData.access_token) {
-    throw new Error(`No access token returned: ${JSON.stringify(tokenData)}`);
+    const tokenData = (await tokenResponse.json()) as { access_token: string };
+
+    if (!tokenData.access_token) {
+      throw new Error(`No access token returned: ${JSON.stringify(tokenData)}`);
+    }
+
+    return tokenData.access_token;
+  } catch (error) {
+    const err = error as Error;
+    throw new Error(`Failed to fetch access token: ${err.message}`);
   }
-
-  return tokenData.access_token;
 };
 
 export const getGraphQLClient = async () => {
