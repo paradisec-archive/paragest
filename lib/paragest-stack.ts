f236937dfcc4ebc042f3f7617897f8adcd117476
John Ferlito <johnf@inodes.org>
2025-06-16 14:33:15 +0200

    WIP: damsmart

--- lib/paragest-stack.ts
+++ lib/paragest-stack.ts
@@ -491,18 +491,9 @@ export class ParagestStack extends cdk.Stack {
         nabuOauthSecret.grantRead(role);
       },
     });
-    const damsmartAddToCatalogStep2 = paragestFargateStep('DAMSmartAddToCatalog2', 'add-to-catalog.ts', {
-      grantFunc: (role) => {
-        ingestBucket.grantRead(role);
-        ingestBucket.grantDelete(role);
-        catalogBucket.grantPut(role);
-        catalogBucket.grantRead(role);
-        nabuOauthSecret.grantRead(role);
-      },
-    });
 
-    const currentFileFlow = sfn.Chain.start(damsmartAddToCatalogStep2);
-    const otherFileFlow = sfn.Chain.start(prepareOtherFileEventStep); // .next(damsmartDetectAndValidateMediaStep);
+    const currentFileFlow = sfn.Chain.start(new sfn.Pass(this, 'NoOp2'));
+    const otherFileFlow = sfn.Chain.start(prepareOtherFileEventStep);
 
     const parallelDAMSmartProcessing = new sfn.Parallel(this, 'ParallelDAMSmartProcessing');
     parallelDAMSmartProcessing.branch(currentFileFlow);
