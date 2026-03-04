b1759d65a2ffc73398f47145961476daa13b0051
John Ferlito <johnf@inodes.org>
2026-03-04 11:02:10 +1100

    fix: temp disable retry code

--- src/lib/rate-limit.ts
+++ src/lib/rate-limit.ts
@@ -1,9 +1,9 @@
-import { DynamoDBClient, UpdateItemCommand, type UpdateItemCommandInput } from '@aws-sdk/client-dynamodb';
+// import { DynamoDBClient, UpdateItemCommand, type UpdateItemCommandInput } from '@aws-sdk/client-dynamodb';
 
-const CONCURRENCY_KEY = 'pk';
-const CURRENT_COUNT_ATTRIBUTE = 'currentCount';
-const CONCURRENCY_LIMIT = 40;
-
+// const CONCURRENCY_KEY = 'pk';
+// const CURRENT_COUNT_ATTRIBUTE = 'currentCount';
+// const CONCURRENCY_LIMIT = 40;
+//
 const DYNAMODB_ENDPOINT = process.env.DYNAMODB_ENDPOINT;
 if (!DYNAMODB_ENDPOINT) {
   throw new Error('Missing env var: DYNAMODB_ENDPOINT');
@@ -14,7 +14,7 @@ if (!CONCURRENCY_TABLE) {
   throw new Error('Missing env var: CONCURRENCY_TABLE');
 }
 
-const dbClient = new DynamoDBClient({ region: 'ap-southeast-2', endpoint: `https://${DYNAMODB_ENDPOINT}` });
+// const dbClient = new DynamoDBClient({ region: 'ap-southeast-2', endpoint: `https://${DYNAMODB_ENDPOINT}` });
 
 // We use a DynamoDB conditional update:
 //   - ConditionExpression => #count < :limit
@@ -22,75 +22,75 @@ const dbClient = new DynamoDBClient({ region: 'ap-southeast-2', endpoint: `https
 // That means "Increment/Decrement currentCount by 1 only if currentCount < 10."
 // If it's already >= 10, DynamoDB will throw a ConditionalCheckFailedException.
 
-const increment = async () => {
-  const incrementParams: UpdateItemCommandInput = {
-    TableName: CONCURRENCY_TABLE,
-    Key: {
-      pk: { S: CONCURRENCY_KEY },
-    },
-    UpdateExpression: 'SET #count = if_not_exists(#count, :zero) + :inc',
-    ConditionExpression: '#count < :limit',
-    ExpressionAttributeNames: {
-      '#count': CURRENT_COUNT_ATTRIBUTE,
-    },
-    ExpressionAttributeValues: {
-      ':inc': { N: '1' },
-      ':limit': { N: CONCURRENCY_LIMIT.toString() },
-      ':zero': { N: '0' },
-    },
-    ReturnValues: 'UPDATED_NEW',
-  };
+// const increment = async () => {
+//   const incrementParams: UpdateItemCommandInput = {
+//     TableName: CONCURRENCY_TABLE,
+//     Key: {
+//       pk: { S: CONCURRENCY_KEY },
+//     },
+//     UpdateExpression: 'SET #count = if_not_exists(#count, :zero) + :inc',
+//     ConditionExpression: '#count < :limit',
+//     ExpressionAttributeNames: {
+//       '#count': CURRENT_COUNT_ATTRIBUTE,
+//     },
+//     ExpressionAttributeValues: {
+//       ':inc': { N: '1' },
+//       ':limit': { N: CONCURRENCY_LIMIT.toString() },
+//       ':zero': { N: '0' },
+//     },
+//     ReturnValues: 'UPDATED_NEW',
+//   };
+//
+//   try {
+//     const response = await dbClient.send(new UpdateItemCommand(incrementParams));
+//
+//     return {
+//       success: true,
+//       count: response.Attributes?.[CURRENT_COUNT_ATTRIBUTE]?.N,
+//     };
+//   } catch (err: unknown) {
+//     const error = err as Error;
+//     if (error.name !== 'ConditionalCheckFailedException') {
+//       throw err;
+//     }
+//
+//     return {
+//       success: false,
+//       count: CONCURRENCY_LIMIT.toString(),
+//     };
+//   }
+// };
+//
+// const decrement = async () => {
+//   const decrementParams: UpdateItemCommandInput = {
+//     TableName: CONCURRENCY_TABLE,
+//     Key: {
+//       pk: { S: CONCURRENCY_KEY },
+//     },
+//     UpdateExpression: 'ADD #count :dec',
+//     ConditionExpression: '#count > :zero',
+//     ExpressionAttributeNames: {
+//       '#count': CURRENT_COUNT_ATTRIBUTE,
+//     },
+//     ExpressionAttributeValues: {
+//       ':dec': { N: '-1' },
+//       ':zero': { N: '0' },
+//     },
+//     ReturnValues: 'UPDATED_NEW',
+//   };
+//
+//   try {
+//     await dbClient.send(new UpdateItemCommand(decrementParams));
+//   } catch (releaseErr) {
+//     console.error('Failed to decrement concurrency counter', releaseErr);
+//   }
+// };
 
-  try {
-    const response = await dbClient.send(new UpdateItemCommand(incrementParams));
-
-    return {
-      success: true,
-      count: response.Attributes?.[CURRENT_COUNT_ATTRIBUTE]?.N,
-    };
-  } catch (err: unknown) {
-    const error = err as Error;
-    if (error.name !== 'ConditionalCheckFailedException') {
-      throw err;
-    }
-
-    return {
-      success: false,
-      count: CONCURRENCY_LIMIT.toString(),
-    };
-  }
-};
-
-const decrement = async () => {
-  const decrementParams: UpdateItemCommandInput = {
-    TableName: CONCURRENCY_TABLE,
-    Key: {
-      pk: { S: CONCURRENCY_KEY },
-    },
-    UpdateExpression: 'ADD #count :dec',
-    ConditionExpression: '#count > :zero',
-    ExpressionAttributeNames: {
-      '#count': CURRENT_COUNT_ATTRIBUTE,
-    },
-    ExpressionAttributeValues: {
-      ':dec': { N: '-1' },
-      ':zero': { N: '0' },
-    },
-    ReturnValues: 'UPDATED_NEW',
-  };
-
-  try {
-    await dbClient.send(new UpdateItemCommand(decrementParams));
-  } catch (releaseErr) {
-    console.error('Failed to decrement concurrency counter', releaseErr);
-  }
-};
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const MAX_RETRIES = 7;
-const BASE_DELAY = 300; // Start with 100ms delay
-const JITTER = 2000; // spread them over 2 seconds
+// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+//
+// const MAX_RETRIES = 7;
+// const BASE_DELAY = 300; // Start with 100ms delay
+// const JITTER = 2000; // spread them over 2 seconds
 
 // biome-ignore lint/suspicious/noExplicitAny: We don't care what we get
 type AnyFunction = (...args: any[]) => Promise<any>;
@@ -98,31 +98,31 @@ type AnyFunction = (...args: any[]) => Promise<any>;
 export const throttle =
   <T extends AnyFunction>(func: T) =>
   async (...args: Parameters<T>): Promise<ReturnType<T>> => {
-    let retries = 0;
+    // let retries = 0;
 
-    while (true) {
-      const result = await increment();
-      if (result.success) {
-        break;
-      }
-
-      if (retries >= MAX_RETRIES) {
-        throw new Error('Too many retries');
-      }
-
-      const delay = BASE_DELAY * 2 ** retries;
-      const jitter = Math.random() * JITTER;
-      console.log(`Retrying in ${delay + jitter}ms`);
-      await sleep(delay + jitter);
-
-      retries += 1;
-    }
+    // while (true) {
+    //   const result = await increment();
+    //   if (result.success) {
+    //     break;
+    //   }
+    //
+    //   if (retries >= MAX_RETRIES) {
+    //     throw new Error('Too many retries');
+    //   }
+    //
+    //   const delay = BASE_DELAY * 2 ** retries;
+    //   const jitter = Math.random() * JITTER;
+    //   console.log(`Retrying in ${delay + jitter}ms`);
+    //   await sleep(delay + jitter);
+    //
+    //   retries += 1;
+    // }
 
     try {
       const response = await func(...args);
 
       return response;
     } finally {
-      decrement();
+      // decrement();
     }
   };
